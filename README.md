03a3a9656c92fd1c3eeb22cab0eff0a234699b00
gustavo.lanzas <gustavo.lanzas@gmail.com>
2018-04-25 13:48:06 -0700

    working Gulp tasks

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # alma-mashup-scss
 Workflow to generate alma mashup css using Gulp/SCSS
+
+## Instructions
+- Copy selector from /src/css stylesheet. 
+- Convert to SCSS
+- Either add to `_customizations.scss` or create a new scss file, and import it into `mashup_new.scss`
+
+## Tasks
+- `gulp` - default task will process scss files into css and then create `skin.zip` archive under `dist`. This is ready to upload to Alma back office. 
+
+## Notes
+This project was done for the PCC Primo New UI launch. It may or may not work in other cases. 
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      zip = require('gulp-zip');
+
+//style paths
+var scssFiles = 'src/scss/**/*.scss',
+    cssDest = 'branding_skin/css/';
+
+gulp.task('styles', function(){
+    gulp.src(scssFiles)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+
+gulp.task('zip', function() {
+    gulp.src('branding_skin/*')
+        .pipe(zip('skin.zip'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['styles', 'zip']);
